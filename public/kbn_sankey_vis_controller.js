9d9783a76d30576beb2a60deb3dd8f1a56a24cb6
Itay Weinberger <itay@joo.la>
2017-04-30 13:42:46 +0300

    align with 4.1

--- public/kbn_sankey_vis_controller.js
+++ public/kbn_sankey_vis_controller.js
@@ -1,154 +1,156 @@
-var observeResize = require('./lib/observe_resize');
-require('./lib/observe_resize');
+import uiModules from 'ui/modules';
 
-define(function (require) {
-  var module = require('ui/modules').get('kibana/kbn_sankey_vis', ['kibana']);
-  var d3 = require('d3');
-  var _ = require('lodash');
-  var $ = require('jquery');
-  var S = require('d3-plugins-sankey');
-  var formatNumber = d3.format(',.0f');
-  var format = function (d) { return formatNumber(d) + ' TWh';  };
+const module = uiModules.get('kibana/kbn_sankey_vis', ['kibana']);
 
-  module.controller('KbnSankeyVisController', function ($scope, $element, $rootScope, Private) {
-    var sankeyAggResponse = Private(require('./lib/agg_response'));
-    const queryFilter = Private(require('ui/filter_bar/query_filter'));
+import d3 from 'd3';
+import _ from 'lodash';
+import $ from 'jquery';
+import S from 'd3-plugins-sankey-fixed';
 
-    var svgRoot = $element[0];
-    var color = d3.scale.category10();
-    var margin = 20;
-    var width;
-    var height;
-    var div;
-    var svg;
+import AggResponseProvider from './lib/agg_response';
 
-    var _updateDimensions = function () {
-      var delta = 18;
-      var w = $element.parent().width();
-      var h = $element.parent().height();
-      if (w) {
-        if (w > delta) {
-          w -= delta;
-        }
-        width = w;
+module.controller('KbnSankeyVisController', function ($scope, $element, $rootScope, Private) {
+  const sankeyAggResponse = Private(AggResponseProvider);
+
+  let svgRoot = $element[0];
+  let color = d3.scale.category10();
+  let margin = 20;
+  let width;
+  let height;
+  let div;
+  let svg;
+  let globalData = null;
+
+  let _updateDimensions = function _updateDimensions() {
+    let delta = 10;
+    let w = $(svgRoot).parent().width() - 10;
+    let h = $(svgRoot).parent().height() - 40;
+    if (w) {
+      if (w > delta) {
+        w -= delta;
       }
-      if (h) {
-        if (h > delta) {
-          h -= delta;
-        }
-        height = h;
+      width = w;
+    }
+    if (h) {
+      if (h > delta) {
+        h -= delta;
       }
-    };
+      height = h;
+    }
+  };
 
 
-    var _buildVis = function (data) {
-      var energy = data.slices;
-      div = d3.select(svgRoot);
+  let _buildVis = function (data) {
+    _updateDimensions();
 
-      if (!energy.nodes.length) return;
+    d3.select(svgRoot).selectAll('svg').remove();
 
-      svg = div.append('svg')
-        .attr('width', width)
-        .attr('height', height + margin)
-        .append('g')
-        .attr('transform', 'translate(0, 0)');
+    let energy = data.slices;
+    div = d3.select(svgRoot);
+    if (!energy.nodes.length) return;
 
-      var sankey = d3.sankey()
-        .nodeWidth(15)
-        .nodePadding(10)
-        .size([width, height]);
-      var path = sankey.link();
-      sankey
-        .nodes(energy.nodes)
-        .links(energy.links)
-        .layout(32);
+    svg = div.append('svg')
+      .attr('width', width)
+      .attr('height', height + margin)
+      .append('g')
+      .attr('transform', 'translate(0, 0)');
 
-      var link = svg.append('g').selectAll('.link')
-        .data(energy.links)
-        .enter().append('path')
-        .attr('class', 'link')
-        .attr('d', path)
-        .style('stroke-width', function (d) { return Math.max(1, d.dy);  })
-        .sort(function (a, b) { return b.dy - a.dy;  });
+    $scope.$on('change:vis', function () {
+      _updateDimensions();
+      _buildVis(globalData);
+    });
 
-      link.append('title')
-        .text(function (d) { return d.source.name + ' → ' + d.target.name + '\n' + format(d.value);  });
+    let sankey = d3.sankey()
+      .nodeWidth(15)
+      .nodePadding(10)
+      .size([width, height]);
 
-      var node = svg.append('g').selectAll('.node')
-        .data(energy.nodes)
-        .enter().append('g')
-        .attr('class', 'node')
-        .attr('transform', function (d) { return 'translate(' + d.x + ',' + d.y + ')';  })
-        .on('click', function(node){
-          console.log('click',node);
-          console.log('fields', energy.fields);
+    let path = sankey.link();
 
-          let searchField = energy.fields[node.name].field;
-          const q2 = {
-            query: {
-              match: {}
-            },
-            meta: {
-              index: energy.fields[node.name].index
-            }
-          };
-          q2.query.match[searchField] = {
-            query: node.name,
-            type: 'phrase'
-          };
-          queryFilter.addFilters([q2]);
+    sankey
+      .nodes(energy.nodes)
+      .links(energy.links)
+      .layout(32);
+
+    let link = svg.append('g').selectAll('.link')
+      .data(energy.links)
+      .enter().append('path')
+      .attr('class', 'link')
+      .attr('d', path)
+      .style('stroke-width', function (d) {
+        return Math.max(1, d.dy);
+      })
+      .sort(function (a, b) {
+        return b.dy - a.dy;
+      });
+
+    link.append('title')
+      .text(function (d) {
+        return d.source.name + ' → ' + d.target.name + '\n' + d.value;
+      });
+
+    let node = svg.append('g').selectAll('.node')
+      .data(energy.nodes)
+      .enter().append('g')
+      .attr('class', 'node')
+      .attr('transform', function (d) {
+        return 'translate(' + d.x + ',' + d.y + ')';
+      })
+      .call(d3.behavior.drag()
+        .origin(function (d) {
+          return d;
         })
-        //.call(d3.behavior.drag()
-        //.origin(function (d) { return d;  })
-        //.on('dragstart', function () { this.parentNode.appendChild(this);  })
-        //.on('drag', dragmove));
+        .on('dragstart', function () {
+          this.parentNode.appendChild(this);
+        })
+        .on('drag', dragmove));
 
-      node.append('rect')
-        .attr('height', function (d) { return d.dy;  })
-        .attr('width', sankey.nodeWidth())
-        .style('fill', function (d) { return d.color = color(d.name);  })
-        .style('stroke', function (d) { return d3.rgb(d.color).darker(2);  })
-        .append('title')
-        .text(function (d) { return d.name + '\n' + format(d.value);  });
+    node.append('rect')
+      .attr('height', function (d) {
+        return d.dy;
+      })
+      .attr('width', sankey.nodeWidth())
+      .style('fill', function (d) {
+        d.color = color(d.name);
+        return d.color;
+      })
+      .style('stroke', function (d) {
+        return d3.rgb(d.color).darker(2);
+      })
+      .append('title')
+      .text(function (d) {
+        return d.name + '\n' + d.value;
+      });
 
-      node.append('text')
-        .attr('x', -6)
-        .attr('y', function (d) { return d.dy / 2;  })
-        .attr('dy', '.35em')
-        .attr('text-anchor', 'end')
-        .attr('transform', null)
-        .text(function (d) { return d.name;  })
-        .filter(function (d) { return d.x < width / 2;  })
-        .attr('x', 6 + sankey.nodeWidth())
-        .attr('text-anchor', 'start');
+    node.append('text')
+      .attr('x', -6)
+      .attr('y', function (d) {
+        return d.dy / 2;
+      })
+      .attr('dy', '.35em')
+      .attr('text-anchor', 'end')
+      .attr('transform', null)
+      .text(function (d) {
+        return d.name;
+      })
+      .filter(function (d) {
+        return d.x < width / 2;
+      })
+      .attr('x', 6 + sankey.nodeWidth())
+      .attr('text-anchor', 'start');
 
-      function dragmove(d) {
-        d3.select(svgRoot).attr('transform', 'translate(' + d.x + ',' + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ')');
-        sankey.relayout();
-        link.attr('d', path);
-      }
-    };
+    function dragmove(d) {
+      d3.select(svgRoot).attr('transform', 'translate(' + d.x + ',' + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ')');
+      sankey.relayout();
+      link.attr('d', path);
+    }
+  };
 
-
-    var _render = window.render = function (data) {
-      d3.select(svgRoot).selectAll('svg').remove();
+  $scope.$watch('esResponse', function (resp) {
+    if (resp) {
+      var data = sankeyAggResponse($scope.vis, resp);
+      globalData = data;
       _buildVis(data);
-    };
-
-    var chartData;
-    $scope.$watch('esResponse', function (resp) {
-      if (resp) {
-        chartData = sankeyAggResponse($scope.vis, resp);
-        _updateDimensions();
-        _render(chartData);
-      }
-    });
-
-    observeResize($element, function () {
-      if (chartData) {
-        _updateDimensions();
-        _render(chartData);
-      }
-    });
+    }
   });
 });
